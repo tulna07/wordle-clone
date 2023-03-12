ad5e82546f4d6411954969851e781a94b5101d38
scoobytux <tu.lna07@gmail.com>
2023-03-12 16:18:22 +0700

    refactor(exercise-5): apply specialization technique to banner component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -6,7 +6,8 @@ import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 // Components
 import GuessInput from "../GuessInput";
 import GuessResults from "../GuessResults/GuessResults";
-import Banner from "../Banner";
+import LostBanner from "../LostBanner/LostBanner";
+import WonBanner from "../WonBanner/WonBanner";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -26,21 +27,8 @@ function Game() {
 
   return (
     <>
-      {lose && (
-        <Banner status="sad">
-          <p>
-            Sorry, the correct answer is <strong>{answer}</strong>.
-          </p>
-        </Banner>
-      )}
-      {win && (
-        <Banner status="happy">
-          <p>
-            <strong>Congratulations!</strong> Got it in{" "}
-            <strong>{guesses.length} guesses</strong>.
-          </p>
-        </Banner>
-      )}
+      {lose && <LostBanner answer={answer} />}
+      {win && <WonBanner numOfGuesses={guesses.length} />}
       <GuessResults items={guesses} answer={answer} />
       <GuessInput onAdd={handleAddGuess} disabled={gameOver} />
     </>
